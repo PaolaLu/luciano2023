e3488d8fc158b595d4f7cfcd4db7fa5fadf5f8be
PaolaLu <paolaluciasepulveda@gmail.com>
2023-05-13 18:09:46 -0300

    clase libro

--- src/Modelos/Libro.php
+++ src/Modelos/Libro.php
@@ -4,4 +4,44 @@ namespace Src\Modelos;
 
 class Libro extends ModeloBase
 {
+private string $isbn;
+private string $titulo;
+private string $autor;
+private int $edicion;
+
+public function __construct(?string $id,string $isbn, string $titulo, string $autor, int $edicion )
+
+{
+    parent::__construct($id);
+    $this->isbn = $isbn ;
+    $this->titulo = $titulo ;
+    $this->autor = $autor ;
+    $this->edicion = $edicion ;
+
 }
+
+public function serializarBd(): array
+{
+    return [
+              'id'=> $this->id,
+              'isbn'=> $this->isbn,
+              'titulo'=> $this->titulo,
+              'autor'=> $this->autor,
+              'edicion'=> $this->edicion,
+    ];
+}
+
+public function serializarJson(): array
+{
+    
+    return [
+        'id'=> $this->id,
+        'isbn'=> $this->isbn,
+        'titulo'=> $this->titulo,
+        'autor'=> $this->autor,
+        'edicion'=> $this->edicion, 
+    ];
+}
+}
+
+
