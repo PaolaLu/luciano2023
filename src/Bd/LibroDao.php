83078a7f4717c7850b01a7b188d93cd3d53252ee
PaolaLu <paolaluciasepulveda@gmail.com>
2023-05-15 19:23:38 -0300

    libro Dao y libro Controlador

--- src/Bd/LibroDao.php
+++ src/Bd/LibroDao.php
@@ -2,6 +2,58 @@
 
 namespace Src\Bd;
 
+use PDO;
+use PDOStatement;
+use Src\Modelos\ModeloBase;
+
 final class LibroDao extends DaoAbstracto
 {
+    public static function listar(): array
+    {
+        $datos=self::ejecutar("SELECT * FROM libros ",[],function(PDO $con,PDOStatement $consulta){
+            return $consulta->fetchALL(PDO::FETCH_NUM);
+        });
+
+        return $datos;
+
+    }
+ public static function buscarPorId(string $id)
+
+ {
+    $query = "SELECT * FROM libros WHERE id =:id LIMIT 1";
+    $parametros = array(":id"=> $id);
+    $datos = self::ejecutar($query, $parametros,function (PDO $con, PDOStatement $consulta){
+        return $consulta->fetch(PDO::FETCH_ASSOC);
+    });
+    return $datos;
+ }
+
+
+ public static function persistir(ModeloBase $libro): void
+ {
+    $query = "INSERT INTO libros (id, isbn, titulo, edicion, autor) VALUE (:id,:isbn, :titulo, :edicion, :autor)";
+    $parametros = $libro->serializarBd();
+    $resultado = self::ejecutar($query,$parametros, function(PDO $con,PDOStatement $consulta){
+
+    });
+ }
+
+public static function actualizar(ModeloBase $instancia): void
+{
+    $query = "UPDATE libros SET isbn=:isbn, titulo=:titulo, edicion=:edicion ,autor=:autor WHERE id=:id";
+    $parametros = $instancia->serializarBd();
+    $resultado = self::ejecutar($query,$parametros,function(PDO $con, PDOStatement $consulta){
+
+    });
 }
+
+
+public static function borrar(string $id): void
+{
+    $sql="DELETE FROM libros WHERE id=:id";
+    $parametros = array(":id"=> $id);
+    $consulta = self::ejecutar($sql,$parametros,function(PDO $con, PDOStatement $consulta){});
+
+}
+}
+
--- src/Controladores/LibroControlador.php
+++ src/Controladores/LibroControlador.php
@@ -2,6 +2,41 @@
 
 namespace Src\Controladores;
 
-class LibroControlador implements ControladorInterface
-{
-}
+use Src\Bd\LibroDao;
+use Src\Modelos\Libro;
+
+    class LibroControlador implements ControladorInterface
+    {
+        public static function listar(): array
+        {
+            $nuevodao = new LibroDao();
+            $libros = $nuevodao->listar();
+            return[$libros];
+        }
+        public static function buscarPorId(string $id): ?array
+        {
+            $nuevodao = new LibroDao();
+            $nuevodao->buscarPorId($id);
+            return [$nuevodao->buscarPorId($id)];
+        }
+        public static function crear(array $parametrosCrudos): array
+        {
+            $libroDao = new LibroDao();
+            $libro = new Libro(null,$parametrosCrudos[0],$parametrosCrudos[1],$parametrosCrudos[2],$parametrosCrudos[3]);
+            $libroDao->persistir($libro);
+            return [$libro];
+        }
+        public static function actualizar(array $parametrosCrudos): array
+        {
+            $nuevodao = new LibroDao();
+            $libro = new Libro($parametrosCrudos[0],$parametrosCrudos[1],$parametrosCrudos[2],$parametrosCrudos[3],$parametrosCrudos[4]);
+            $nuevodao->actualizar($libro);
+            return[$libro];
+
+        }
+        public static function borrar(string $id): void
+        {
+            $nuevodao =new LibroDao();
+            $nuevodao->borrar($id);
+        }
+    }
