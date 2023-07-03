7a248e8af2d6d0d361df0e7b891468ed26a15bd8
PaolaLu <paolaluciasepulveda@gmail.com>
2023-07-03 20:49:25 -0300

    socio actualizado con navbar y footer

--- src/Controladores/SocioControlador.php
+++ src/Controladores/SocioControlador.php
@@ -8,8 +8,8 @@ use Src\Modelos\Socio;
 class SocioControlador implements ControladorInterface
 {
     public static function listar(): array{
-        $nuevodao= new SocioDao();
-        $sociosDao= $nuevodao->listar();
+
+        $sociosDao= SocioDao::listar();
         $socios=[];
         foreach($sociosDao as $socio){
             $serializado= $socio->serializarBD();
@@ -19,34 +19,31 @@ class SocioControlador implements ControladorInterface
     }
     public static function buscarPorId(string $id): ?array{
 
-        $nuevodao= new SocioDao();
-        $socioDao=$nuevodao->buscarPorId($id);
+        
+        $socioDao= SocioDao::buscarPorId($id);
         $socio=[];
         $serializado=$socioDao->serializarJson();
         $socio[]=$serializado;
-        return $socio;
 
-        
+        return $socio;
+       
     }
     public static function crear(array $parametrosCrudos): array{
 
-        $sociodao=new SocioDao();
         $socio= new Socio(null,$parametrosCrudos[0],$parametrosCrudos[1],$parametrosCrudos[2]);
-        $sociodao->persistir($socio);
+        SocioDao::persistir($socio);
    
         return [$socio];
     }
     public static function actualizar(array $parametrosCrudos): array
     {
-        $nuevodao= new SocioDao();
         $socio= new Socio($parametrosCrudos[0],$parametrosCrudos[1],$parametrosCrudos[2],$parametrosCrudos[3]);
-        $nuevodao->actualizar($socio);
+        SocioDao::actualizar($socio);
         return [$socio];
     }
     public static function borrar(string $id): void{
 
-        $nuevodao= new SocioDao();
-        $nuevodao->borrar($id);
+        SocioDao::borrar($id);
 
     }
 }
